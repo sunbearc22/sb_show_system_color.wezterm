6bb61aa05e9fb99c831781885bdc814af4c6a2d5
sunbearc22 <anon@anonymous.com>
2025-11-03 23:43:27 +0800

    First commit.

---
+++ README.md
@@ -0,0 +1,47 @@
+# sb_show_system_color.wezterm
+
+ This plugin obtains the System's theme and color and the shades, tints, triadic-colors,
+ complementary-color and analogous-colors of the System's color to configure your Wezterm's:
+   - config.color_scheme
+   - config.colors for:
+     - window fg & bg
+     - cursor
+     - selection
+     - scroll_bar thumb
+     - split
+     - launcher
+   - config.integrated_title_button_color
+
+Also, it provides you with a wezterm.GLOBAL.system table with the System's theme and color,
+and the shades, tints, triadic-colors, complementary-color and analogous-colors of the System's
+color. You can use this table to configure the color of other WezTerm features. You can see their
+values in your WezTerm log file that is located in the `$XDG_RUNTIME_DIR/wezterm` directory.
+
+**NOTE: This plugin only works on Ubuntu version>=24.04.**
+
+## Installation and Usage
+
+```lua
+local wezterm = require("wezterm")
+
+local config = {}
+
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
+
+-- Add these lines (to install and use the plugin):
+swp = wezterm.plugin.require("https://github.com/sunbearc22/sb_show_system_color.wezterm.git")
+swp.apply_to_config(config, {})
+
+return config
+```
+
+## Update
+
+Press <kbd>CTRL</kbd><kbd>SHIFT</kbd><kbd>L</kbd> and run `wezterm.plugin.update_all()`.
+
+## Removal
+
+1. Press <kbd>CTRL</kbd><kbd>SHIFT</kbd><kbd>L</kbd> and run `wezterm.plugin.list()`.
+2. Delete the `"plugin_dir"` directory of this plugin.
---
+++ plugin/init.lua
@@ -0,0 +1,153 @@
+--[[
+This Plugin does the following:
+1. Initialize a wezterm.GLOBAL.system table with default value for:
+    theme, color, shades, tints, triadic, complementary and analogous
+  - These default values serve as fallback values.
+  - Fields "theme" and "color" stores the system theme and color.
+  - The next five fields are derived from "color" using functions in func_c.lua.
+2. Obtain the System's theme and color via a custom python script that works only
+   for Ubuntu>=24.04.
+3. Update all the fields of wezterm.GLOBAL.system and use it to configure:
+   - config.color_scheme
+   - config.colors for:
+     - window fg & bg
+     - cursor
+     - selection
+     - scroll_bar thumb
+     - split
+     - launcher
+   - config.integrated_title_button_color
+
+Written by: sunbearc22
+Tested on: Ubuntu 24.04.3, wezterm 20251025-070338-b6e75fd7
+]]
+local M = {}
+
+local wezterm = require("wezterm")
+local func_c = require("func_c")
+
+---@param config unknown
+---@param opts { nshades: number?, ntints: number?, nanalogous: number? }
+-- Module Function to apply to config
+function M.apply_to_config(config, opts)
+  local nshades = opts.nshades or 10
+  local ntints = opts.ntints or 10
+  local nanalogous = opts.nanalogous or 3
+
+  -- Checks for opts
+  if nshades < 10 then
+    wezterm.log_info("[COLOR] Warning: nshades < 10. Default value will instead be used.")
+    nshades = 10
+  end
+  if ntints < 10 then
+    wezterm.log_info("[COLOR] Warning: ntints < 10. Default value will instead be used.")
+    ntints = 10
+  end
+  if nanalogous < 10 then
+    wezterm.log_info("[COLOR] Warning: nanalogous < 3. Default value will instead be used.")
+    nanalogous = 10
+  end
+
+  -- Local Function to update wezterm.GLOBAL.system table with Ubuntu >=24.04
+  -- theme, color, shades, tints, triadic, complementary and analogous colors
+  local function update_GLOBAL_system_theme_color()
+    -- Default state of wezterm.GLOBAL.system table
+    if not wezterm.GLOBAL.system then
+      wezterm.GLOBAL.system = {
+        theme = "Yaru-purple-dark",
+        color = "#7764d8",
+        shades = func_c.get_shades_of("#7764d8", nshades),
+        tints = func_c.get_tints_of("#7764d8", ntints),
+        triadic = func_c.get_triadic_colors_of("#7764d8"),
+        complementary = func_c.get_complementary_color_of("#7764d8"),
+        analogous = func_c.get_analogous_colors_of("#7764d8", nanalogous),
+      }
+    end
+
+    -- Use custom python script to get Ubuntu >=24.04 theme and color
+    local pyscript = wezterm.config_dir .. "/get_ubuntu_24.04_theme_color.py"
+    -- wezterm.log_info("[COLOR] pyscript = " .. pyscript)
+    local success, stdout, stderr = wezterm.run_child_process({ "python3", pyscript })
+
+    -- What to do when python script fails
+    if not success then
+      wezterm.log_errot("[COLOR] Failed to run Python script: " .. tostring(stderr))
+    end
+
+    -- Split stdout into individual lines, removes leading and trailing
+    -- whitespace from each line, filter out empty lines and store data
+    -- as an array
+    local lines = {}
+    for line in stdout:gmatch("[^\r\n]+") do
+      line = line:match("^%s*(.-)%s*$")
+      if line ~= "" then
+        table.insert(lines, line)
+      end
+    end
+
+    -- Update GLOBAL.system table with Ubuntu's theme, color, shades, tints,
+    -- triadic, complementary and analogius colors
+    if #lines == 2 then
+      wezterm.GLOBAL.system.theme = lines[1]
+      wezterm.GLOBAL.system.color = string.lower(lines[2])
+      local ucolor = wezterm.GLOBAL.system.color
+      wezterm.GLOBAL.system.shades = func_c.get_shades_of(ucolor, nshades)
+      wezterm.GLOBAL.system.tints = func_c.get_tints_of(ucolor, ntints)
+      wezterm.GLOBAL.system.triadic = func_c.get_triadic_colors_of(ucolor)
+      wezterm.GLOBAL.system.complementary = func_c.get_complementary_color_of(ucolor)
+      wezterm.GLOBAL.system.analogous = func_c.get_analogous_colors_of(ucolor, nanalogous)
+    else
+      wezterm.log_errot("[COLOR] Expected exactly 2 lines from Python script, got " .. #lines)
+    end
+
+    -- Debug: Print all values
+    wezterm.log_info(
+      "[COLOR] wezterm.GLOBAL.system:- theme=" .. wezterm.GLOBAL.system.theme
+      .. ", color=" .. wezterm.GLOBAL.system.color
+    )
+
+    local function string_hexcodes(icolors)
+      local cc = ""
+      for _, c in ipairs(icolors) do
+        cc = cc .. " " .. c
+      end
+      return cc
+    end
+
+    wezterm.log_info("wezterm.GLOBAL.system.shades " .. string_hexcodes(wezterm.GLOBAL.system.shades))
+    wezterm.log_info("wezterm.GLOBAL.system.tints " .. string_hexcodes(wezterm.GLOBAL.system.tints))
+    wezterm.log_info("wezterm.GLOBAL.system.triadic " .. string_hexcodes(wezterm.GLOBAL.system.triadic))
+    wezterm.log_info("wezterm.GLOBAL.system.complementary " .. string_hexcodes(wezterm.GLOBAL.system.complementary))
+    wezterm.log_info("wezterm.GLOBAL.system.analogous " .. string_hexcodes(wezterm.GLOBAL.system.analogous))
+  end
+
+  -- Local Function returns a color scheme for a given system theme
+  local function get_color_scheme_for(theme)
+    if string.lower(theme):find("dark") then
+      return "Catppuccin Mocha"
+    else
+      return "Catppuccin Latte"
+    end
+  end
+
+  -- Configure colors
+  update_GLOBAL_system_theme_color()
+  config.color_scheme = get_color_scheme_for(wezterm.GLOBAL.system.theme)
+  config.colors = {
+    foreground = wezterm.GLOBAL.system.tints[5],   -- The default text color
+    background = wezterm.GLOBAL.system.shades[10], -- The default background color
+    cursor_bg = wezterm.GLOBAL.system.color,
+    cursor_fg = wezterm.GLOBAL.system.triadic[3],
+    cursor_border = wezterm.GLOBAL.system.shades[8],
+    compose_cursor = "gold",
+    selection_fg = wezterm.GLOBAL.system.complementary[2],
+    selection_bg = wezterm.GLOBAL.system.shades[9],
+    scrollbar_thumb = wezterm.GLOBAL.system.color,
+    split = wezterm.GLOBAL.system.shades[6],
+    launcher_label_bg = { AnsiColor = "Black" },                      -- (*Since: Nightly Builds Only*)
+    launcher_label_fg = { Color = wezterm.GLOBAL.system.triadic[2] }, -- (*Since: Nightly Builds Only*)
+  }
+  config.integrated_title_button_color = wezterm.GLOBAL.system.color
+end
+
+return M
